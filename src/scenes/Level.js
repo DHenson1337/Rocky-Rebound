9e61921b0b6ace8308f54b6b0a68f4b544c773c2
dhenson <davonhensontech@gmail.com>
2025-02-16 14:53:28 -0500

    First commit for platformer game using phaser inspiried by Metroid-inspired infinite Jumper guide

---
+++ src/scenes/Level.js
@@ -0,0 +1,42 @@
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import PlatformPrefab from "../prefabs/PlatformPrefab.js";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+    // Write your code here.
+    /* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// platformPrefab
+		const platformPrefab = new PlatformPrefab(this, 300, 374);
+		this.add.existing(platformPrefab);
+
+		this.events.emit("scene-awake");
+	}
+
+	/* START-USER-CODE */
+
+  // Write more your code here
+
+  create() {
+    this.editorCreate();
+  }
+
+  /* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
